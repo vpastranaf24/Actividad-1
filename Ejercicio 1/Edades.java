03afe5f1f9a297ac1449b6602ca34ddbcbe195bc
vpastranaf24 <vpastranaf@unal.edu.co>
2026-09-17 20:47:38 -0500

    Edades.java

--- Ejercicio 1/Edades.java
+++ Ejercicio 1/Edades.java
@@ -1,21 +1,16 @@
-package Actividad1_ejercicio1;
+package Edades;
 
-import java.util.Scanner;
-public class CalcularEdades {
-
-    public static void main(String[] args){ 
-     double edjuan, edalber, edana, edmama;
+public class Edades {
     
-     Scanner scanner = new Scanner (System.in);
-     System.out.print("How old is Juan?");
-     edjuan = scanner.nextDouble();
-     
-     edalber = Edades.calcular_edalber(edjuan);
-     edana = Edades.calcular_edana(edjuan);
-     edmama = Edades.calcular_edmama(edjuan,edalber,edana);
-     System.out.println("La edad de la mama es: "+ edmama);
-     System.out.println("La edad de la Juan es: "+ edjuan);
-     System.out.println("La edad de la Alber es: "+ edalber);
-     System.out.println("La edad de la Ana es: "+ edana);  
-    }
+    public static double calcular_edalber(double edjuan) {
+		return 2* edjuan/3;
+	}
+
+    public static double calcular_edana(double edjuan) {
+		return 4* edjuan/3;
+	}
+
+    public static double calcular_edmama(double edjuan,double edalber,double edana) {
+		return edjuan + edalber + edana;
+	}
 }

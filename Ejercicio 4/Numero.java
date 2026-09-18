a9a9b8db7c3806812132cc7d571f1d46336c59b4
vpastranaf24 <vpastranaf@unal.edu.co>
2026-09-17 21:20:02 -0500

    Numero.java

---
+++ Ejercicio 4/Numero.java
@@ -0,0 +1,12 @@
+package Actividad1_ejercicio4;
+
+public class Numero {
+
+    public static double calcularcuadrado(double numero) {
+        return Math.pow(numero, 2);
+    }
+
+    public static double calcularcubo(double numero) {
+        return Math.pow(numero, 3);
+    }
+}

9b3f7b90e9becf55627505a4c3b8afd8fdb6d17b
vpastranaf24 <vpastranaf@unal.edu.co>
2026-09-17 20:59:06 -0500

    Operaciones.java

---
+++ Ejercicio 2/Operaciones.java
@@ -0,0 +1,14 @@
+package Operaciones;
+
+public class Operaciones {
+    
+    public static double operacion1(double suma, double x) {
+        return suma + x;
+    }
+    public static double operacion2(double x, double y) {
+        return x + Math.pow(y, 2);
+    }
+    public static double operacion3(double suma, double x, double y) {
+        return suma + (x / y);
+    }
+}

daf77c4e0682a728052ac1600e827f08501a47bb
vpastranaf24 <vpastranaf@unal.edu.co>
2026-09-17 21:00:38 -0500

    Seguimiento.java

---
+++ Ejercicio 2/Seguimiento.java
@@ -0,0 +1,25 @@
+package Seguimiento;
+
+import java.util.Scanner;
+public class Seguimiento {    
+
+    public static void main(String[] args) {
+        double suma, x, y;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el valor de x: ");
+        x = scanner.nextDouble();
+
+        System.out.print("Ingrese el valor de suma: ");
+        suma = scanner.nextDouble();
+        suma = Operaciones.operacion1(suma, x);
+
+        System.out.print("Ingrese el valor de Y: ");
+        y = scanner.nextDouble();
+
+        x = Operaciones.operacion2(x, y);
+        suma = Operaciones.operacion3(suma, x, y);
+
+        System.out.println("El valor de la suma es:" + suma);
+    }
+}

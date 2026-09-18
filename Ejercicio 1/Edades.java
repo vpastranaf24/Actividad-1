ad764ec8246550086cacb41fa160f70cd846c467
vpastranaf24 <vpastranaf@unal.edu.co>
2026-09-17 20:21:54 -0500

    Create Edades.java

---
+++ Ejercicio 1/Edades.java
@@ -0,0 +1,16 @@
+package Actividad1_ejercicio1;
+
+public class Edades {
+    
+    public static double calcular_edalber(double edjuan) {
+		return 2* edjuan/3;
+	}
+
+    public static double calcular_edana(double edjuan) {
+		return 4* edjuan/3;
+	}
+
+    public static double calcular_edmama(double edjuan,double edalber,double edana) {
+		return edjuan + edalber + edana;
+	}
+}
